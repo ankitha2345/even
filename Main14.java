fcc76e9be2ee71eb4515bbd713ae03c0b072c26a
ankitha2345 <pankitha2005@gmail.com>
2025-06-04 16:09:56 +0530

    Add files via upload

---
+++ Main14.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+public class Main
+{
+    public static void main(String[]args)
+    {
+        Stack s=new Stack<>();
+        s.push(1);
+        s.push(2);
+        System.out.print(s.peek());
+        System.out.print(s.pop());
+    }
+}
